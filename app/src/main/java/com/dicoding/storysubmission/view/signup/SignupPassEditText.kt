cb89f68d085eb29218acda1c80528f03e8e270e7
Imam Munif <imammunif17@gmail.com>
2024-05-13 13:56:07 +0700

    replace text edit with custom text edit & add warning

---
+++ app/src/main/java/com/dicoding/storysubmission/view/signup/SignupPassEditText.kt
@@ -0,0 +1,47 @@
+package com.dicoding.storysubmission.view.signup
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class SignupPassEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs), View.OnTouchListener {
+
+    init {
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            // Do nothing.
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, after: Int) {
+                if (s.toString().length < 8) {
+                    setError("Inputan tidak boleh kurang dari 8 karakter", null)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            // Do nothing
+            }
+        })
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Input your new password"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+    override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+        return false
+    }
+}
