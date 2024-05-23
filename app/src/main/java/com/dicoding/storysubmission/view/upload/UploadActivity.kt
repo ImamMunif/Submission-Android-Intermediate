10fe506a5750d279832e64b39cddc645ac12ec95
Imam Munif <imammunif17@gmail.com>
2024-05-23 12:06:24 +0700

    add back button in tool app bar UploadActivity

--- app/src/main/java/com/dicoding/storysubmission/view/upload/UploadActivity.kt
+++ app/src/main/java/com/dicoding/storysubmission/view/upload/UploadActivity.kt
@@ -5,8 +5,17 @@ import android.os.Bundle
 import com.dicoding.storysubmission.R
 
 class UploadActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+
+        supportActionBar?.title = "Upload"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
     }
 }
