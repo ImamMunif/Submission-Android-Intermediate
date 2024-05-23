24842e2d6e4a882374bca80facd6d2fb737d4b95
Imam Munif <imammunif17@gmail.com>
2024-05-23 11:33:52 +0700

    create UploadActivity UI

---
+++ app/src/main/java/com/dicoding/storysubmission/view/upload/UploadActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.storysubmission.view.upload
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.storysubmission.R
+
+class UploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+    }
+}
