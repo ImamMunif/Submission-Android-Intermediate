e9f6608c2bec742e9253b05159820c99ffb482f5
Imam Munif <imammunif17@gmail.com>
2024-05-24 09:11:22 +0700

    permission & get imageUri from camera & gallery

--- app/src/main/java/com/dicoding/storysubmission/view/upload/UploadActivity.kt
+++ app/src/main/java/com/dicoding/storysubmission/view/upload/UploadActivity.kt
@@ -1,21 +1,104 @@
 package com.dicoding.storysubmission.view.upload
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.storysubmission.R
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.dicoding.storysubmission.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityUploadBinding
+
+    private var currentImageUri: Uri? = null
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.title = "Upload"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+
+            binding.layoutButton.galleryButton.setOnClickListener { startGallery() }
+            binding.layoutButton.cameraButton.setOnClickListener { startCamera() }
+            Log.d("Debug: onCreate", "onCreate started")
+            Log.d("Debug: onCreate", "currentImageURI: $currentImageUri")
+        }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+          Log.d("Log: UploadActivity", "startGallery: function startGallery triggered")
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            Log.d("Log: UploadActivity", "launcherGallery: currentImageUri: $uri")
+            showImage()
+        } else {
+            Log.d("Log: UploadActivity", "launcherGallery: No media selected")
+        }
+    }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri!!)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            Log.d("Log: UploadActivity", "launcherCamera: currentImageURI: $currentImageUri")
+            showImage()
+        } else {
+            Log.d("Log: UploadActivity", "launcherCamera: No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            binding.previewImageView.setImageURI(it)
+              Log.d("Log: UploadActivity", "showImage: Uri selected: $it")
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         onBackPressedDispatcher.onBackPressed()
         return true
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 }
